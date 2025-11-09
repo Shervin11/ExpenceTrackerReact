a143d96b0deb5da995cccef1b22ecc256f073199
Shervin <shqodirov11@gmail.com>
2025-11-10 01:27:21 +0500

    change index.css

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -96,7 +96,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-50 pb-6">
+    <div className="pb-6">
       <div className="bg-white border-b border-gray-200 sticky top-0 z-10">
         <div className="max-w-4xl mx-auto px-4 py-5 flex justify-between items-center">
           <div>
