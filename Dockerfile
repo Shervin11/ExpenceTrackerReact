eb6a1e81bacf54576c243cfde9991689bf7f2019
Shervin <shqodirov11@gmail.com>
2025-11-09 19:04:09 +0500

    change docker, compose and vite config

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ COPY . .
 EXPOSE 5173
 
 # Запуск dev-сервера
-CMD ["npm", "run", "dev"]
+CMD ["npm", "run", "dev", "--", "--host", "0.0.0.0"]
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["tracker.52-221-219-64.nip.io"],
+  },
 });
